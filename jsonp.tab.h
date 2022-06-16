c1c2702ca8893801c9e9367044de84dda8132edc
mlnutt <spam.4.me@outlook.com>
2022-06-16 10:48:51 -0700

    More jsonp.tab.h compatability..

--- jsonp.tab.h
+++ jsonp.tab.h
@@ -27,17 +27,23 @@ extern YYSTYPE yytext;
 extern int yylex(void);
 
 #ifndef YY_SIZE_T
-extern int yyleng;
+	extern int yyleng;
 #else
-#ifndef YY_TYPEDEF_YY_SIZE_T
-#define YY_TYPEDEF_YY_SIZE_T
-typedef size_t yy_size_t;
-#endif
-#if DESKTOP_SESSION == ubuntu
-extern int yyleng;
-#else
-extern yy_size_t yyleng;
-#endif
+	#ifndef YY_TYPEDEF_YY_SIZE_T
+		#define YY_TYPEDEF_YY_SIZE_T
+		typedef size_t yy_size_t;
+	#endif
+
+	#ifdef DESKTOP_SESSION
+		#if DESKTOP_SESSION == ubuntu
+			extern int yyleng;
+		#else
+			extern yy_size_t yyleng;
+		#endif
+	#else
+		extern yy_size_t yyleng;
+	#endif
+
 #endif
 
 extern FILE* yyin;
